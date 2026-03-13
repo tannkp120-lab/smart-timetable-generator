43667b9896715d77d51c1fd0a8e51b4bc06bb813
tannkp120-lab <tannkp120@gmail.com>
2026-03-13 22:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -977,3 +977,7 @@ def generate_timetable():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
